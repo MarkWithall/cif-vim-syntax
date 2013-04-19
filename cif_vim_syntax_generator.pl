cde2ad6c3715c1c61594ad06c1965239a087cca8
Mark Withall <Mark Withall github@markwithall.com>
2013-04-19 13:09:35 +0100

    Changed to use list of hashes

--- cif_vim_syntax_generator.pl
+++ cif_vim_syntax_generator.pl
@@ -3,17 +3,19 @@
 use strict;
 use warnings;
 use Carp;
+use List::Util 'sum';
 
 sub create_section {
-    my ($name, $record_identity, $names, $lengths, $highligh_prefix) = @_;
+    my ($name, $record_identity, $fields, $highligh_prefix) = @_;
     print "\" $name\n";
-    create_syntax($record_identity, $names, $lengths);
-    create_links($record_identity, $names, $highligh_prefix);
+    create_syntax($record_identity, [map {$_->{name}} @$fields], [map {$_->{size}} @$fields]);
+    create_links($record_identity, [map {$_->{name}} @$fields], $highligh_prefix);
 }
 
 sub create_syntax {
     my ($identity, $names, $lengths) = @_;
     croak "Must have same number of record names as lengths" if ($#$names != $#$lengths);
+    croak "Sum of field lengths must be 78" if (sum(@$lengths) != 78);
     syn_match(element($identity, 'RecordIdentity'), '^' . uc($identity), element($identity, $$names[0]), 0);
     for my $i (0 .. $#$names) {
         my $next = ($i < $#$names) ? element($identity, $$names[$i+1]) : '';
@@ -71,34 +73,118 @@ endif
 VIM
 
 # BS
-my @bs_names = qw(TransactionType TrainUid DateRunsFrom DateRunsTo DaysRun BankHolidayRunning TrainStatus TrainCategory TrainIdentity Headcode CourseIndicator TrainServiceCode PortionId PowerType TimingLoad Speed OperatingCharacteristics TrainClass Sleepers Reservations ConnectionIndicator CateringCode ServiceBranding Spare StpIndicator);
-my @bs_lengths = qw(1 6 6 6 7 1 1 2 4 4 1 8 1 3 4 3 6 1 1 1 1 4 4 1 1);
-create_section('Basic Schedule', 'bs', \@bs_names, \@bs_lengths, 'bs');
+my @bs_fields = (
+    {name => 'TransactionType', size => 1},
+    {name => 'TrainUid', size => 6},
+    {name => 'DateRunsFrom', size => 6},
+    {name => 'DateRunsTo', size => 6},
+    {name => 'DaysRun', size => 7},
+    {name => 'BankHolidayRunning', size => 1},
+    {name => 'TrainStatus', size => 1},
+    {name => 'TrainCategory', size => 2},
+    {name => 'TrainIdentity', size => 4},
+    {name => 'Headcode', size => 4},
+    {name => 'CourseIndicator', size => 1},
+    {name => 'TrainServiceCode', size => 8},
+    {name => 'PortionId', size => 1},
+    {name => 'PowerType', size => 3},
+    {name => 'TimingLoad', size => 4},
+    {name => 'Speed', size => 3},
+    {name => 'OperatingCharacteristics', size => 6},
+    {name => 'TrainClass', size => 1},
+    {name => 'Sleepers', size => 1},
+    {name => 'Reservations', size => 1},
+    {name => 'ConnectionIndicator', size => 1},
+    {name => 'CateringCode', size => 4},
+    {name => 'ServiceBranding', size => 4},
+    {name => 'Spare', size => 1},
+    {name => 'StpIndicator', size => 1},
+);
+create_section('Basic Schedule', 'bs', \@bs_fields, 'bs');
 
 # BX
-my @bx_names = qw(TractionClass UicCode AtocCode ApplicableTimetableCode ReservedFieldRsid ReservedFieldDataSource Spare);
-my @bx_lengths = qw(4 5 2 1 8 1 57);
-create_section('Basic Schedule Extra Detail', 'bx', \@bx_names, \@bx_lengths, '');
+my @bx_fields = (
+    {name => 'TractionClass', size => 4},
+    {name => 'UicCode', size => 5},
+    {name => 'AtocCode', size => 2},
+    {name => 'ApplicableTimetableCode', size => 1},
+    {name => 'ReservedFieldRsid', size => 8},
+    {name => 'ReservedFieldDataSource', size => 1},
+    {name => 'Spare', size => 57},
+);
+create_section('Basic Schedule Extra Detail', 'bx', \@bx_fields, '');
 
 # LO
-my @lo_names = qw(Location ScheduledDeparture PublicDeparture Platform Line EngineeringAllowance PathingAllowance Activity PerformanceAllowance Spare);
-my @lo_lengths = qw(8 5 4 3 3 2 2 12 2 37);
-create_section('Origin Location', 'lo', \@lo_names, \@lo_lengths, 'lo');
+my @lo_fields = (
+    {name => 'Location',                size => 8},
+    {name => 'ScheduledDeparture',       size => 5},
+    {name => 'PublicDeparture',         size => 4},
+    {name => 'Platform',                size => 3},
+    {name => 'Line',                    size => 3},
+    {name => 'EngineeringAllowance',    size => 2},
+    {name => 'PathingAllowance',        size => 2},
+    {name => 'Activity',                size => 12},
+    {name => 'PerformanceAllowance',    size => 2},
+    {name => 'Spare',                   size => 37},
+);
+create_section('Origin Location', 'lo', \@lo_fields, 'lo');
 
 # LI
-my @li_names = qw(Location ScheduledArrival ScheduledDeparture ScheduledPass PublicArrival PublicDeparture Platform Line Path Activity EngineeringAllowance PathingAllowance PerformanceAllowance Spare);
-my @li_lengths = qw(8 5 5 5 4 4 3 3 3 12 2 2 2 20);
-create_section('Intermediate Location', 'li', \@li_names, \@li_lengths, '');
+my @li_fields = (
+    {name => 'Location', size => 8},
+    {name => 'ScheduledArrival', size => 5},
+    {name => 'ScheduledDeparture', size => 5},
+    {name => 'ScheduledPass', size => 5},
+    {name => 'PublicArrival', size => 4},
+    {name => 'PublicDeparture', size => 4},
+    {name => 'Platform', size => 3},
+    {name => 'Line', size => 3},
+    {name => 'Path', size => 3},
+    {name => 'Activity', size => 12},
+    {name => 'EngineeringAllowance', size => 2},
+    {name => 'PathingAllowance', size => 2},
+    {name => 'PerformanceAllowance', size => 2},
+    {name => 'Spare', size => 20},
+);
+create_section('Intermediate Location', 'li', \@li_fields, '');
 
 # CR
-my @cr_names = qw(Location TrainCategory TrainIdentity Headcode CourseIndicator TrainServiceCode PortionId PowerType TimingLoad Speed OperatingCharacteristics TrainClass Sleepers Reservations ConnectionIndicator CateringCode ServiceBranding TractionClass UicCode ReservedFieldRsid Spare);
-my @cr_lengths = qw(8 2 4 4 1 8 1 3 4 3 6 1 1 1 1 4 4 4 5 8 5);
-create_section('Change En Route', 'cr', \@cr_names, \@cr_lengths, '');
+my @cr_fields = (
+    {name => 'Location', size => 8},
+    {name => 'TrainCategory', size => 2},
+    {name => 'TrainIdentity', size => 4},
+    {name => 'Headcode', size => 4},
+    {name => 'CourseIndicator', size => 1},
+    {name => 'TrainServiceCode', size => 8},
+    {name => 'PortionId', size => 1},
+    {name => 'PowerType', size => 3},
+    {name => 'TimingLoad', size => 4},
+    {name => 'Speed', size => 3},
+    {name => 'OperatingCharacteristics', size => 6},
+    {name => 'TrainClass', size => 1},
+    {name => 'Sleepers', size => 1},
+    {name => 'Reservations', size => 1},
+    {name => 'ConnectionIndicator', size => 1},
+    {name => 'CateringCode', size => 4},
+    {name => 'ServiceBranding', size => 4},
+    {name => 'TractionClass', size => 4},
+    {name => 'UicCode', size => 5},
+    {name => 'ReservedFieldRsid', size => 8},
+    {name => 'Spare', size => 5},
+);
+create_section('Change En Route', 'cr', \@cr_fields, '');
 
 # LT
-my @lt_names = qw(Location ScheduledArrival PublicArrival Platform Path Activity Spare);
-my @lt_lengths = qw(8 5 4 3 3 12 43);
-create_section('Terminating Location', 'lt', \@lt_names, \@lt_lengths, 'lt');
+my @lt_fields = (
+    {name => 'Location', size => 8},
+    {name => 'ScheduledArrival', size => 5},
+    {name => 'PublicArrival', size => 4},
+    {name => 'Platform', size => 3},
+    {name => 'Path', size => 3},
+    {name => 'Activity', size => 12},
+    {name => 'Spare', size => 43},
+);
+create_section('Terminating Location', 'lt', \@lt_fields, 'lt');
 
 # FOOTER
 print <<'VIM';
